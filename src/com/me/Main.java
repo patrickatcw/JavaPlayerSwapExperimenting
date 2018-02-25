14357afb3126f22f8aa99282d085a1278beed5b4
patrickatcw <pj8392@yahoo.com>
2018-02-24 19:50:57 -0500

    starting program from JavaPlayerSwap

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,4 +1,32 @@
 package com.me;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Team blue = new Team("Bob", "Bill", "Bo", "Ben", "Buck");
+        Team red = new Team("Rick", "Rob", "Rita", "Renee", "Rudy");
+        Team orange = new Team("Otter", "Othello", "Oscar", "Octavian", "Oren");
+        Team neon = new Team("Nancy", "Ned", "Nelly", "Nick", "Natalie");
+        Team purple = new Team("Pete", "Pam", "Pandit", "Phun-shu", "Pogo");
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("Here are the team rosters for the player swap;");
+        System.out.println();
+        printTeam(blue, "Blue");
+        printTeam(red, "Red");
+        printTeam(orange, "Orange");
+        printTeam(neon, "Neon");
+        printTeam(purple, "Purple");
+        System.out.println("-------------------------------------------------");
+
+    }
+
+    private static void printTeam(Team team, String teamName) {
+        System.out.println("Team " + teamName + ": " + team.getPlayer1() + ", " + team.getPlayer2() + ", " +
+                team.getPlayer3() + ", " + team.getPlayer4() + ", " + team.getPlayer5());
+
+    }
+
+
 }
--- src/com/me/Team.java
+++ src/com/me/Team.java
@@ -1,4 +1,48 @@
 package com.me;
 
 public class Team {
+
+    private String player1;
+    private String player2;
+    private String player3;
+    private String player4;
+    private String player5;
+
+    public Team(String player1, String player2, String player3,
+                String player4, String player5){
+
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player3 = player3;
+        this.player4 = player4;
+        this.player5 = player5;
+
+    }
+
+    //create method to switch players
+    //i want to replace one player with another player on different teams, or
+    //bring in a new player not on any teams currently
+    public void replacePlayer(String playerNameRemove, String playerNameAdd){
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public String getPlayer3() {
+        return player3;
+    }
+
+    public String getPlayer4() {
+        return player4;
+    }
+
+    public String getPlayer5() {
+        return player5;
+    }
+
 }
